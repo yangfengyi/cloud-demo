eae205615f21370e8404d114e0eb40948698a1c3
yangfengyi <sheapfly@yeah.net>
2022-04-03 22:37:42 +0800

    feat: add response cache

--- sw.ts
+++ sw.ts
@@ -1,6 +1,15 @@
 // learn from https://www.youtube.com/watch?v=sOq92prx00w
 import { precacheAndRoute } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import { NetworkFirst } from 'workbox-strategies'
 
 declare const self: ServiceWorkerGlobalScope;
 
+// 缓存静态资源
 precacheAndRoute(self.__WB_MANIFEST);
+
+// 缓存接口的数据
+registerRoute(
+  ({ url }) => url.origin === 'https://cloud-worker-demo.solojacker-worker.workers.dev/',
+  new NetworkFirst({ cacheName: 'api-responses' })
+);
