a0658d4f0ff2799003977c52f5d24578bbf79755
yangfengyi <sheapfly@yeah.net>
2022-04-03 19:05:59 +0800

    feat: add fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.css'
 
 export default function App() {
-  return <div>views</div>
+
+  const [ name, setName ] = useState('')
+
+  useEffect(() => {
+    fetch(
+      'https://odd-dust-cfe6.solojacker-worker.workers.dev/',
+      {
+        method: 'GET'
+      }
+    )
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      setName(data.name)
+    })
+  })
+
+  return (
+    <div>
+      <h1>cloud demo</h1>
+      <p>resived data: {name}</p>
+    </div>
+  )
 }
